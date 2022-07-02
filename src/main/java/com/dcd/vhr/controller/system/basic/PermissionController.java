5574cb92ac7c128275bb3aae69ddca18a4fcd6b4
MrDecadent <mrdecadent1314@gmail.com>
2022-07-03 03:07:22 +0800

    添加修改菜单角色接口

--- src/main/java/com/dcd/vhr/controller/system/basic/PermissionController.java
+++ src/main/java/com/dcd/vhr/controller/system/basic/PermissionController.java
@@ -1,12 +1,11 @@
 package com.dcd.vhr.controller.system.basic;
 
 import com.dcd.vhr.model.Menu;
+import com.dcd.vhr.model.RespBean;
 import com.dcd.vhr.model.Role;
 import com.dcd.vhr.service.MenuService;
 import com.dcd.vhr.service.RoleService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -29,5 +28,19 @@ public class PermissionController {
     @GetMapping("/menus")
     public List<Menu> getAllMenus(){
         return menuService.getAllMenus();
+    }
+
+    @GetMapping("/mids/{rid}")
+    public List<Integer> getMidsByRid(@PathVariable("rid") Integer rid){
+        return menuService.getMidsByRid(rid);
+    }
+
+    @PutMapping("/")
+    public RespBean updateMenuRole(Integer rid,Integer[] mids){
+        if (menuService.updateMenuRole(rid,mids) == mids.length){
+            return RespBean.ok("更新成功");
+        }else {
+            return RespBean.error("更新失败");
+        }
     }
 }
--- src/main/java/com/dcd/vhr/service/MenuService.java
+++ src/main/java/com/dcd/vhr/service/MenuService.java
@@ -1,11 +1,13 @@
 package com.dcd.vhr.service;
 
 import com.dcd.vhr.mapper.MenuMapper;
+import com.dcd.vhr.mapper.MenuRoleMapper;
 import com.dcd.vhr.model.Hr;
 import com.dcd.vhr.model.Menu;
 import com.dcd.vhr.model.Role;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -15,6 +17,9 @@ public class MenuService {
 
     @Resource
     private MenuMapper menuMapper;
+
+    @Resource
+    MenuRoleMapper menuRoleMapper;
 
     public List<Menu> getMenusIdByHrId(){
         //getPrincipal()当前登录的用户对象
@@ -29,5 +34,18 @@ public class MenuService {
 
     public List<Menu> getAllMenus() {
         return menuMapper.getAllMenus();
+    }
+
+
+    public List<Integer> getMidsByRid(Integer rid) {
+        return menuMapper.getMidsByRid(rid);
+    }
+
+    @Transactional  //添加事务
+    public Integer updateMenuRole(Integer rid, Integer[] mids) {
+        //先删除
+        menuRoleMapper.deleteByRid(rid);
+        //再添加
+        return menuRoleMapper.insertRecord(rid,mids);
     }
 }
